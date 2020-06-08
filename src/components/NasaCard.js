e85684e68bd5a06457782adfc7666015392eb0a0
alan5123 <alanparker5123@gmail.com>
2020-06-08 17:31:46 -0400

    test

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,12 +1,44 @@
-import React from "react"
+import React, { useState } from 'react';
+import styled from "styled-components"
+import { Button, Fade } from 'reactstrap';
 
+
+
+const NasaTitle = styled.h2 `
+color:white;
+`
+const NasaImage = styled.img `
+max-width:70%;
+max-height:1000px;
+
+`
+const PicDate = styled.p `
+font-weight:bold;
+color:white;
+`
+const PicExplanation = styled.p `
+margin:5%;
+color:white;
+
+`
 const NasaCard = props => {
+    const [fadeIn, setFadeIn] = useState(false);
+
+    const toggle = () => setFadeIn(!fadeIn);
+
     return (
+        
         <div className= 'Photo-list'>
-            <h2>{props.title}</h2>
-<img src ={props.image} alt= "space " />
-    <p>{props.date}</p>
-    <p>{props.explanation}</p>
+            <NasaTitle>{props.title}</NasaTitle>
+<NasaImage src ={props.image} alt= "NASA pic of the day " />
+    <PicDate>{props.date}</PicDate>
+    <div>
+            <Button color="info" onClick={toggle}>What Am i looking at?</Button>
+            <Fade in={fadeIn} tag="h5" className="mt-3">
+            <PicExplanation>{props.explanation}</PicExplanation>
+            </Fade>
+        </div>
+   
         </div>
     )
 }
